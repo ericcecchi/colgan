c7f0160cc6f9f8f1e475197f4c145ddf684c1fb9
Eric Cecchi <eric.cecchi@gmail.com>
2012-03-26 23:46:45 -0500

    Page load optimizations, image permissions, image bug on Retina displays
    Also added G-forms xml for importing.

--- wordpress/wp-content/themes/colgan/header.php
+++ wordpress/wp-content/themes/colgan/header.php
@@ -8,28 +8,21 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css">
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-		<script src="<?php bloginfo('template_url'); ?>/js/jquery.js"></script>
+		
+	  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	  <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.1.min.js"><\/script>')</script>
+	  
+		<!-- Typekit -->
 		<script src="//use.typekit.com/vpn3lfq.js"></script>
 		<script>try{Typekit.load();}catch(e){}</script>
+		
 		<!--[if lt IE 9]>
 		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
-<!-- Begin WordPress -->
-<?php wp_head(); ?>
-<!-- End Wordpress -->
-	<script type="text/javascript">
-	
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-30348046-1']);
-	  _gaq.push(['_trackPageview']);
-	
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	
-	</script>
+		<!-- Begin WordPress -->
+		<?php wp_head(); ?>
+		<!-- End Wordpress -->
 	</head>
 	<body <?php body_class(); ?>>
 		<header role="banner">

563e579ee0b374aff6880584a29c1b608f79955b
Eric Cecchi <eric.cecchi@gmail.com>
2012-03-28 14:28:10 -0500

    More deletes that didn't go through in last commit

--- wordpress/wp-content/themes/minimalistic/functions/options/js/slide_toggle_menu.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('.settings').slideUp();
-	jQuery('.slideToggle h3').click(function(){
-		jQuery(this).parent().next('.settings').slideToggle('slow');
-	});
-});

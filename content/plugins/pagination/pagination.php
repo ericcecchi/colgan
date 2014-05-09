6e0c207d9395c96a5898fac4d2c327af5e9497a2
Eric Cecchi <eric.cecchi@gmail.com>
2014-05-08 23:51:35 -0500

    Fixed ugly styling of buttons.

--- content/plugins/pagination/pagination.php
+++ content/plugins/pagination/pagination.php
@@ -16,17 +16,17 @@ function pag_enqueue_styles() {
 	wp_enqueue_style( 'pag_style' );
 }
 
-/* To use with a custom loop, pass 
-		$my_loop->max_num_pages 
+/* To use with a custom loop, pass
+		$my_loop->max_num_pages
 	as the first parameter */
 
 function pagination($pages = '', $range = 4)
-{  
-     $showitems = ($range * 2)+1;  
- 
+{
+     $showitems = ($range * 2)+1;
+
      global $paged;
      if(empty($paged)) $paged = 1;
- 
+
      if($pages == '')
      {
          global $wp_query;
@@ -35,14 +35,14 @@ function pagination($pages = '', $range = 4)
          {
              $pages = 1;
          }
-     }   
- 
+     }
+
      if(1 != $pages)
      {
          echo "<div class=\"pagination\"><span class=\"pages\">Page ".$paged." of ".$pages."</span>";
          if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a class=\"pages\" href='".get_pagenum_link(1)."'>&laquo; First</a>";
          if($paged > 1 && $showitems < $pages) echo "<a class=\"pages\" href='".get_pagenum_link($paged - 1)."'>&lsaquo; Previous</a>";
- 
+
          for ($i=1; $i <= $pages; $i++)
          {
              if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
@@ -50,9 +50,9 @@ function pagination($pages = '', $range = 4)
                  echo ($paged == $i)? "<span class=\"current\">".$i."</span>":"<a href='".get_pagenum_link($i)."' class=\"inactive\">".$i."</a>";
              }
          }
- 
-         if ($paged < $pages && $showitems < $pages) echo "<a class=\"pages\" href=\"".get_pagenum_link($paged + 1)."\">Next &rsaquo;</a>";  
-         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a class=\"pages\" href='".get_pagenum_link($pages)."'>Last &raquo;</a>";
+
+         if ($paged < $pages && $showitems < $pages) echo "<a class=\"pages\" href=\"".get_pagenum_link($paged + 1)."\">Next</a>";
+         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a class=\"pages\" href='".get_pagenum_link($pages)."'>Last</a>";
          echo "</div>\n";
      }
 }
